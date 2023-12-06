6b3f1399bb2c4f518ed205cea30c54188e5632cc
jiyoung choi <sarahchoi.webdeveloper@outlook.com>
2023-12-06 16:57:33 +1100

    better way to use useEffect and mado in one page

--- reveiwPage/src/App.jsx
+++ reveiwPage/src/App.jsx
@@ -1,35 +1,92 @@
 import data from "./data";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import Review from "./Review";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
+import { FaQuoteRight } from "react-icons/fa";
 
+// const App = () => {
+//   const [review, setReview] = useState(data)
+//   const [currentDisplay, setCurrentDisplay] = useState(1)
+// const nextReview= ()=>{
+//      if( currentDisplay+1 > review.length ){
+//       setCurrentDisplay(1)
+//      }
+//      else{
+//       setCurrentDisplay(currentDisplay+1)
+//      }
+// }
+// const randomNum =()=>{
+//         const newNum =Math.floor(Math.random() * review.length+1)
+//        return setCurrentDisplay(newNum)
+// }
+// const preReview= ()=>{
+//   if( currentDisplay-1 < 1 ){
+//    setCurrentDisplay(review.length)
+//   }
+//   else{
+//    setCurrentDisplay(currentDisplay-1)
+//   }
+// }
+//     return (
+//       <main className="">
+//         <Review review={review} currentDisplay={currentDisplay} nextReview={nextReview} preReview={preReview} randomNum={randomNum}/>
+
+//       </main>
+//     )
+// };
 const App = () => {
-  const [review, setReview] = useState(data)
-  const [currentDisplay, setCurrentDisplay] = useState(1)
-const nextReview= ()=>{
-     if( currentDisplay+1 > review.length ){
-      setCurrentDisplay(1)
-     }
-     else{
-      setCurrentDisplay(currentDisplay+1)
-     }
-}
-const randomNum =()=>{
-        const newNum =Math.floor(Math.random() * review.length+1)
-       return setCurrentDisplay(newNum)
-}
-const preReview= ()=>{
-  if( currentDisplay-1 < 1 ){
-   setCurrentDisplay(review.length)
-  }
-  else{
-   setCurrentDisplay(currentDisplay-1)
-  }
-}
-    return (
-      <main className="">
-        <Review review={review} currentDisplay={currentDisplay} nextReview={nextReview} preReview={preReview} randomNum={randomNum}/>
-        
-      </main>
-    )
+  const [currNum, setCurrNum] = useState(0);
+  const [currentData, setCurrentData] = useState(data[currNum]);
+  const { name, job, text, image } = currentData;
+  const randomNum = () => {
+    let newNum = Math.floor(Math.random() * data.length);
+    console.log(newNum);
+    if (newNum === currNum) {
+      newNum = (currNum + 1) % data.length;
+    }
+    setCurrNum(newNum);
+  };
+
+  useEffect(() => {
+    setCurrentData(data[currNum]);
+  }, [currNum, data]);
+
+  const nextReview = () => {
+    if (currNum + 1 > data.length) {
+      return setCurrNum(1);
+    } else {
+      return setCurrNum(currNum + 1);
+    }
+  };
+  const preReview = () => {
+    if (currNum - 1 < 0) {
+      setCurrNum(data.length - 1);
+    } else {
+      setCurrNum(currNum - 1);
+    }
+  };
+  return (
+    <main>
+      <div className="review">
+        {" "}
+        <div className="img-container">
+          <img className="person-img" src={image} />
+          <div className="quote-icon">
+            <FaQuoteRight />
+          </div>
+        </div>
+        <h3 className="auther">{name}</h3>
+        <h4 className="job">{job}</h4>
+        <div className="info">{text}</div>
+        <div className="btn-container">
+          <SlArrowLeft className="prev-btn" onClick={preReview} />
+          <SlArrowRight className="next-btn" onClick={nextReview} />
+        </div>
+        <button className="btn" onClick={randomNum}>
+          Suprise Me
+        </button>
+      </div>
+    </main>
+  );
 };
 export default App;
