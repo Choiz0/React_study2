13aab0d576fc5ea97e172c624ec7dbd9e3d13159
jiyoung choi <sarahchoi.webdeveloper@outlook.com>
2023-12-12 16:34:52 +1100

    navbar.. responsive

---
+++ navbar/src/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useRef, useEffect } from 'react';
+import logo from "./logo.svg"; 
+import { FaBars } from "react-icons/fa";
+import { links } from "./data";
+
+const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+    const [linkHeight, setLinkHeight] = useState(0);
+
+    const toggleLinks = () => {
+        setShowLinks(!showLinks);
+    };
+
+    useEffect(() => {
+        if (linksRef.current) {
+            setLinkHeight(linksRef.current.getBoundingClientRect().height);
+        }
+    }, [showLinks]);
+
+    const linkStyle = {
+        height: showLinks ? `${linkHeight}px` : '0px',
+    };
+
+    return (
+        <div className='nav-center'>
+            <div className="nav-header">
+                <img src={logo} alt="Logo" className="logo" /> 
+                <button className="nav-toggle" onClick={toggleLinks}>
+                    <FaBars/>
+                </button>
+            </div>
+            <div className="links-container" ref={linksContainerRef} style={linkStyle}>
+                <ul className="links" ref={linksRef}>
+                    {links.map((it) => {
+                        const { id, url, text } = it;
+                        return (
+                            <li key={id}>
+                                <a href={url}>{text}</a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
